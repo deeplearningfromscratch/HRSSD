b6e8357b9b83c80962d22d9ab61b779c72834c6b
jason <jason@furiosa.ai>
2019-11-30 17:46:58 +0900

    test non-patch model

---
+++ test_ssd_non-patch_network.py
@@ -0,0 +1,112 @@
+import os
+import math
+import random
+
+import numpy as np
+import tensorflow as tf
+import cv2
+
+slim = tf.contrib.slim
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import sys
+from nets import ssd_vgg_512, ssd_common, np_methods
+from preprocessing import ssd_vgg_preprocessing
+from notebooks import visualization
+
+# TensorFlow session: grow memory when needed. TF, DO NOT USE ALL MY GPU MEMORY!!!
+gpu_options = tf.GPUOptions(allow_growth=True)
+config = tf.ConfigProto(log_device_placement=False, gpu_options=gpu_options)
+
+
+# Input placeholder.
+
+def inference(net_shape, isess, num_use=None):
+    data_format = 'NHWC'
+    img_input = tf.placeholder(tf.uint8, shape=(None, None, 3))
+    # Evaluation pre-processing: resize to SSD net shape.
+    image_pre, labels_pre, bboxes_pre, bbox_img = ssd_vgg_preprocessing.preprocess_for_eval(
+        img_input, None, None, net_shape, data_format, resize=ssd_vgg_preprocessing.Resize.WARP_RESIZE)
+    image_4d = tf.expand_dims(image_pre, 0)
+
+    # Define the SSD model.
+    reuse = True if 'ssd_net' in locals() else None
+    reuse = tf.AUTO_REUSE
+    ssd_net = ssd_vgg_512.SSDNet()
+    with slim.arg_scope(ssd_net.arg_scope(data_format=data_format)):
+        predictions, localisations, _, _ = ssd_net.net(image_4d, is_training=False, reuse=reuse)
+
+    predictions = predictions[:num_use]
+    localisations = localisations[:num_use]
+    # Restore SSD model.
+    # ckpt_filename = 'checkpoints/ssd_300_vgg.ckpt'
+    # ckpt_filename = 'checkpoints/VGG_VOC0712_SSD_300x300_iter_120000.ckpt'
+    ckpt_filename = 'checkpoints/VGG_VOC0712_SSD_512x512_ft_iter_120000.ckpt'
+    isess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    saver.restore(isess, ckpt_filename)
+
+    # SSD default anchor boxes.
+    ssd_anchors = ssd_net.anchors(net_shape)
+
+    return img_input, image_4d, predictions, localisations, bbox_img, ssd_anchors
+
+
+# Main image processing routine.
+def process_image_non_patch(img, select_threshold=0.5, nms_threshold=.45, net_shape=(300, 300)):
+    # Run SSD network.
+    classes = []
+    scores = []
+    bboxes = []
+    isess = tf.InteractiveSession(config=config)
+    img_input, image_4d, predictions, localisations, bbox_img, ssd_anchors = inference((720, 1280), isess, num_use=-2)
+    rimg, rpredictions, rlocalisations, rbbox_img = isess.run([image_4d, predictions, localisations, bbox_img],
+                                                              feed_dict={img_input: img})
+
+    # Get classes and bboxes from the net outputs.
+    rclasses, rscores, rbboxes = np_methods.ssd_bboxes_select(
+        rpredictions, rlocalisations, ssd_anchors,
+        select_threshold=select_threshold, img_shape=net_shape, num_classes=21, decode=True)
+
+    rbboxes = np_methods.bboxes_clip(rbbox_img, rbboxes)
+
+    classes.append(rclasses)
+    scores.append(rscores)
+    bboxes.append(rbboxes)
+
+    img_input, image_4d, predictions, localisations, bbox_img, ssd_anchors = inference((300, 300), isess)
+    rimg, rpredictions, rlocalisations, rbbox_img = isess.run([image_4d, predictions, localisations, bbox_img],
+                                                              feed_dict={img_input: img})
+
+    # Get classes and bboxes from the net outputs.
+    rclasses, rscores, rbboxes = np_methods.ssd_bboxes_select(
+        rpredictions, rlocalisations, ssd_anchors,
+        select_threshold=select_threshold, img_shape=net_shape, num_classes=21, decode=True)
+    classes.append(rclasses)
+    scores.append(rscores)
+    bboxes.append(rbboxes)
+
+    rclasses = np.concatenate(classes)
+    rscores = np.concatenate(scores)
+    rbboxes = np.concatenate(bboxes)
+
+    rbboxes = np_methods.bboxes_clip(rbbox_img, rbboxes)
+    rclasses, rscores, rbboxes = np_methods.bboxes_sort(rclasses, rscores, rbboxes, top_k=400)
+    rclasses, rscores, rbboxes = np_methods.bboxes_nms(rclasses, rscores, rbboxes, nms_threshold=nms_threshold)
+    # Resize bboxes to original image shape. Note: useless for Resize.WARP!
+    rbboxes = np_methods.bboxes_resize(rbbox_img, rbboxes)
+    return rclasses, rscores, rbboxes
+
+
+# Test on some demo image and visualize output.
+path = 'bdd_img/'
+image_names = sorted(os.listdir(path))
+print(image_names)
+
+img = mpimg.imread(path + image_names[0])
+rclasses, rscores, rbboxes = process_image_non_patch(img)
+
+# visualization.bboxes_draw_on_img(img, rclasses, rscores, rbboxes, visualization.colors_plasma)
+visualization.plt_bboxes(img, rclasses, rscores, rbboxes)
