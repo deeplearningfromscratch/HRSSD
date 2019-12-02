6fccac1f0ece0dc9187b9432ef4055e573a1b0cb
Chogrylls <jason@furiosa.ai>
2019-12-02 22:44:18 +0900

    img pic save

--- notebooks/visualization.py
+++ notebooks/visualization.py
@@ -86,7 +86,7 @@ def bboxes_draw_on_img(img, classes, scores, bboxes, colors, thickness=2):
 # =========================================================================== #
 # Matplotlib show...
 # =========================================================================== #
-def plt_bboxes(img, classes, scores, bboxes, figsize=(10,10), linewidth=1.5):
+def plt_bboxes(img, classes, scores, bboxes, figsize=(10,10), linewidth=1.5, demo=True):
     """Visualize bounding boxes. Largely inspired by SSD-MXNET!
     """
     fig = plt.figure(figsize=figsize)
@@ -94,26 +94,44 @@ def plt_bboxes(img, classes, scores, bboxes, figsize=(10,10), linewidth=1.5):
     height = img.shape[0]
     width = img.shape[1]
     colors = dict()
-    for i in range(classes.shape[0]):
-        cls_id = int(classes[i])
-        if cls_id >= 0:
-            score = scores[i]
-            if cls_id not in colors:
-                colors[cls_id] = (random.random(), random.random(), random.random())
-            ymin = int(bboxes[i, 0] * height)
-            xmin = int(bboxes[i, 1] * width)
-            ymax = int(bboxes[i, 2] * height)
-            xmax = int(bboxes[i, 3] * width)
-            rect = plt.Rectangle((xmin, ymin), xmax - xmin,
-                                 ymax - ymin, fill=False,
-                                 edgecolor=colors[cls_id],
-                                 linewidth=linewidth)
-            plt.gca().add_patch(rect)
-            class_name = str(cls_id)
-            plt.gca().text(xmin, ymin - 2,
-                           '{:s} | {:.3f}'.format(class_name, score),
-                           bbox=dict(facecolor=colors[cls_id], alpha=0.5),
-                           fontsize=12, color='white')
+    if demo is not True:
+        for i in range(classes.shape[0]):
+            cls_id = int(classes[i])
+            if cls_id >= 0:
+                score = scores[i]
+                if cls_id not in colors:
+                    colors[cls_id] = (random.random(), random.random(), random.random())
+                ymin = int(bboxes[i, 0] * height)
+                xmin = int(bboxes[i, 1] * width)
+                ymax = int(bboxes[i, 2] * height)
+                xmax = int(bboxes[i, 3] * width)
+                rect = plt.Rectangle((xmin, ymin), xmax - xmin,
+                                     ymax - ymin, fill=False,
+                                     edgecolor=colors[cls_id],
+                                     linewidth=linewidth)
+                plt.gca().add_patch(rect)
+                class_name = str(cls_id)
+                plt.gca().text(xmin, ymin - 2,
+                               '{:s} | {:.3f}'.format(class_name, score),
+                               bbox=dict(facecolor=colors[cls_id], alpha=0.5),
+                               fontsize=12, color='white')
+    else:
+        for i in range(classes.shape[0]):
+            cls_id = int(classes[i])
+            if cls_id >= 0:
+                score = scores[i]
+                if cls_id not in colors:
+                    colors[cls_id] = (1, 0, 0)
+                ymin = int(bboxes[i, 0] * height)
+                xmin = int(bboxes[i, 1] * width)
+                ymax = int(bboxes[i, 2] * height)
+                xmax = int(bboxes[i, 3] * width)
+                rect = plt.Rectangle((xmin, ymin), xmax - xmin,
+                                     ymax - ymin, fill=False,
+                                     edgecolor=colors[cls_id],
+                                     linewidth=linewidth)
+                plt.gca().add_patch(rect)
+                plt.savefig('demo.png')
     plt.show()
 
 def plt_bboxes_tf(img, scores, bboxes, figsize=(10, 10), linewidth=1.5):
--- test_ssd_network.py
+++ test_ssd_network.py
@@ -38,7 +38,7 @@ with slim.arg_scope(ssd_net.arg_scope(data_format=data_format)):
 
 # Restore SSD model.
 # ckpt_filename = 'checkpoints/ssd_300_vgg.ckpt'
-ckpt_filename = 'checkpoints/ssd_300_vgg.ckpt'
+ckpt_filename = 'checkpoints/VGG_VOC0712_SSD_300x300_ft_iter_120000.ckpt'
 isess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 saver.restore(isess, ckpt_filename)
@@ -67,7 +67,7 @@ def process_image(img, select_threshold=0.5, nms_threshold=.45, net_shape=(300, 
 
 
 
-# Test on some demo image and visualize output.
+# Test on some demo image and visualize output
 path = 'demo/'
 image_names = sorted(os.listdir(path))
 
